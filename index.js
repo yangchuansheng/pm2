d2e3695d884c3d483cd7c926a78245225b29a462
vmarchaud <contact@vmarchaud.fr>
2017-01-19 23:57:38 +0100

    (semistandard) start migration

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ process.env.PM2_PROGRAMMATIC = 'true';
 
 var API = require('./lib/API.js');
 
-module.exports = new API;
+module.exports = new API();
 module.exports.custom = API;
--- paths.js
+++ paths.js
@@ -6,18 +6,16 @@
  */
 
 var debug = require('debug')('pm2:paths');
-var p     = require('path');
+var p = require('path');
 
-function getDefaultPM2Home() {
+function getDefaultPM2Home () {
   var PM2_ROOT_PATH;
 
-  if (process.env.PM2_HOME)
-    PM2_ROOT_PATH = process.env.PM2_HOME;
-  else if (process.env.HOME && !process.env.HOMEPATH)
+  if (process.env.PM2_HOME) { PM2_ROOT_PATH = process.env.PM2_HOME; } else if (process.env.HOME && !process.env.HOMEPATH) {
     PM2_ROOT_PATH = p.resolve(process.env.HOME, '.pm2');
-  else if (process.env.HOME || process.env.HOMEPATH)
+  } else if (process.env.HOME || process.env.HOMEPATH) {
     PM2_ROOT_PATH = p.resolve(process.env.HOMEDRIVE, process.env.HOME || process.env.HOMEPATH, '.pm2');
-  else {
+  } else {
     console.error('[PM2][Initialization] Environment variable HOME (Linux) or HOMEPATH (Windows) are not set!');
     console.error('[PM2][Initialization] Defaulting to /etc/.pm2');
     PM2_ROOT_PATH = p.resolve('/etc', '.pm2');
@@ -27,36 +25,37 @@ function getDefaultPM2Home() {
   return PM2_ROOT_PATH;
 }
 
-module.exports = function(PM2_HOME) {
-  if (!PM2_HOME)
-    PM2_HOME = getDefaultPM2Home()
+module.exports = function (PM2_HOME) {
+  if (!PM2_HOME) {
+    PM2_HOME = getDefaultPM2Home();
+  }
 
   var pm2_file_stucture = {
-    PM2_HOME                 : PM2_HOME,
-    PM2_ROOT_PATH            : PM2_HOME,
+    PM2_HOME: PM2_HOME,
+    PM2_ROOT_PATH: PM2_HOME,
 
-    PM2_CONF_FILE            : p.join(PM2_HOME, 'conf.js'),
-    PM2_MODULE_CONF_FILE     : p.join(PM2_HOME, 'module_conf.json'),
+    PM2_CONF_FILE: p.join(PM2_HOME, 'conf.js'),
+    PM2_MODULE_CONF_FILE: p.join(PM2_HOME, 'module_conf.json'),
 
-    PM2_LOG_FILE_PATH        : p.join(PM2_HOME, 'pm2.log'),
-    PM2_PID_FILE_PATH        : p.join(PM2_HOME, 'pm2.pid'),
+    PM2_LOG_FILE_PATH: p.join(PM2_HOME, 'pm2.log'),
+    PM2_PID_FILE_PATH: p.join(PM2_HOME, 'pm2.pid'),
 
-    DEFAULT_PID_PATH         : p.join(PM2_HOME, 'pids'),
-    DEFAULT_LOG_PATH         : p.join(PM2_HOME, 'logs'),
-    DUMP_FILE_PATH           : p.join(PM2_HOME, 'dump.pm2'),
+    DEFAULT_PID_PATH: p.join(PM2_HOME, 'pids'),
+    DEFAULT_LOG_PATH: p.join(PM2_HOME, 'logs'),
+    DUMP_FILE_PATH: p.join(PM2_HOME, 'dump.pm2'),
 
-    DAEMON_RPC_PORT          : p.join(PM2_HOME, 'rpc.sock'),
-    DAEMON_PUB_PORT          : p.join(PM2_HOME, 'pub.sock'),
-    INTERACTOR_RPC_PORT      : p.join(PM2_HOME, 'interactor.sock'),
+    DAEMON_RPC_PORT: p.join(PM2_HOME, 'rpc.sock'),
+    DAEMON_PUB_PORT: p.join(PM2_HOME, 'pub.sock'),
+    INTERACTOR_RPC_PORT: p.join(PM2_HOME, 'interactor.sock'),
 
-    INTERACTOR_LOG_FILE_PATH : p.join(PM2_HOME, 'agent.log'),
-    INTERACTOR_PID_PATH      : p.join(PM2_HOME, 'agent.pid'),
-    INTERACTION_CONF         : p.join(PM2_HOME, 'agent.json5')
+    INTERACTOR_LOG_FILE_PATH: p.join(PM2_HOME, 'agent.log'),
+    INTERACTOR_PID_PATH: p.join(PM2_HOME, 'agent.pid'),
+    INTERACTION_CONF: p.join(PM2_HOME, 'agent.json5')
   };
 
   if (process.platform === 'win32' ||
       process.platform === 'win64') {
-    //@todo instead of static unique rpc/pub file custom with PM2_HOME or UID
+    // @todo instead of static unique rpc/pub file custom with PM2_HOME or UID
     pm2_file_stucture.DAEMON_RPC_PORT = '\\\\.\\pipe\\rpc.sock';
     pm2_file_stucture.DAEMON_PUB_PORT = '\\\\.\\pipe\\pub.sock';
     pm2_file_stucture.INTERACTOR_RPC_PORT = '\\\\.\\pipe\\interactor.sock';
